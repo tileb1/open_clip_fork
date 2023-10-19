298e8ff32119041963f9f4da1fffacde8591251c
tileb1 <43468258+tileb1@users.noreply.github.com>
2023-10-19 19:16:16 +0200

    f

--- src/training/main.py
+++ src/training/main.py
@@ -70,8 +70,8 @@ def get_latest_checkpoint(path: str, remote : bool):
 def main(args):
     print('Will resnet now')
     import torchvision.models
-    test_model = torchvision.models.__dict__['resnet50']().cuda(self.args.gpu)
-    print(test_model(torch.rand(4, 3, 224, 224).cuda(self.args.gpu)))
+    test_model = torchvision.models.__dict__['resnet50']().cuda(os.environ['LOCAL_RANK'])
+    print(test_model(torch.rand(4, 3, 224, 224).cuda(os.environ['LOCAL_RANK'])))
 
     args = parse_args(args)
 
