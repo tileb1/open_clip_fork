020e96dedb2ee5ac2f6f49bcbaaf28de26dacea5
tileb1 <43468258+tileb1@users.noreply.github.com>
2023-10-19 17:53:04 +0200

    run_script

---
+++ submit_script.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#SBATCH --wait-all-nodes=1
+#SBATCH --job-name=open_clip
+#SBATCH --account=project_465000330
+#SBATCH --cpus-per-task=7
+#SBATCH --exclusive
+#SBATCH --gpus-per-node=8
+#SBATCH --mem=448GB
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --open-mode=append
+#SBATCH --partition=standard-g
+#SBATCH --signal=USR2@120
+#SBATCH --time=60
+#SBATCH --wckey=submitit
+module load LUMI/22.08
+module load partition/G
+
+echo "LD_LIBRARY_PATH"
+echo $LD_LIBRARY_PATH
+echo "PATH"
+echo $PATH
+
+export MIOPEN_USER_DB_PATH=/tmp/${USER}-miopen-cache-${SLURM_JOB_ID}
+export MIOPEN_CUSTOM_CACHE_DIR=${MIOPEN_USER_DB_PATH}
+export SINGULARITYENV_NCCL_SOCKET_IFNAME=hsn0,hsn1,hsn2,hsn3
+
+export NCCL_DEBUG=INFO
+export NCCL_SOCKET_IFNAME=hsn0,hsn1,hsn2,hsn3
+export NCCL_NET_GDR_LEVEL=3
+
+export FI_CXI_DISABLE_CQ_HUGETLB=1
+
+  if [ $SLURM_LOCALID -eq 0 ] ; then
+    ls /dev/shm/ | awk '!/data/' | xargs -i rm -rf  /dev/shm/{}
+    rocm-smi || true
+  else
+    sleep 2
+  fi
+
+export SRUN_CPUS_PER_TASK=$SLURM_CPUS_PER_TASK
+export TORCH_DISTRIBUTED_DEBUG="DETAIL"
+
+master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+export MASTER_ADDR=$master_addr
+
+export PYTHONPATH="$PYTHONPATH:$PWD/src"
+srun --unbuffered python -u src/training/main.py \
+    --save-frequency 1 \
+    --report-to wandb \
+    --train-data="/scratch/project_465000727/datasets/img2dataset/mscoco/{00000..00058}.tar" \
+    --warmup 2000 \
+    --batch-size=256 \
+    --epochs=32 \
+    --workers=7 \
+    --model ViT-B-32 \
+    --name "ViT-B-32-Vanilla" \
+    --seed 0 \
+    --local-loss \
+    --gather-with-grad
